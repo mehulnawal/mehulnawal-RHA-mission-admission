2601ad91318bd95f5ca3133e637707826c74e351
Mehul Nawal <your.email@example.com>
2025-04-16 16:13:00 +0530

    wesbite updated

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let menuIcon = document.getElementById("menu-icon");
 let toggleIcon = document.getElementById("toggleIcon");
 let faXmark = document.getElementById("faXmark");
+let body = document.getElementsByTagName("body");
 
 menuIcon.addEventListener("click", function () {
     toggleIcon.classList.add("toggleIcon-Toggle");
+    body.classList.add("bodyToggle")
+
 });
 
 faXmark.addEventListener("click", function () {
     toggleIcon.classList.remove("toggleIcon-Toggle");
+    body.classList.remove("bodyToggle")
 });
 
